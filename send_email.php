46e188b684d79592a5aae4f79940b1786ccb37ab
Rei-sama077 <rei.wysteria17@gmail.com>
2025-05-18 17:36:12 +0800

    Adding php  to access on gmail

---
+++ send_email.php
@@ -0,0 +1,44 @@
+<?php
+
+if(empty($_POST["name"]) || empty($_POST["email"]) || empty($_POST["message"])) {
+    die("Please fill all required fields");
+}
+
+$name = htmlspecialchars($_POST["name"]);
+$email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+$phone = htmlspecialchars($_POST["phone"] ?? '');
+$message = htmlspecialchars($_POST["message"]);
+
+require "vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+$mail = new PHPMailer(true);
+
+
+try {
+$mail->SMTPDebug = SMTP::DEBUG_SERVER; 
+$mail ->isSMTP();
+$mail->SMTPAuth = true;
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+$mail->Port = 465;
+
+$mail->Username = "marjorie.matilos20@gmail.com";
+$mail->Password = "gvmt resk gdfs rzzl";
+
+$mail->setFrom($email, $name);
+$mail->addAddress("marjorie.matilos18@gmail.com", "Marjorie");
+
+$mail->Subject = $phone;
+$mail->Body = $message;
+
+$mail->send();
+
+echo "email sent";
+
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
